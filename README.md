e6e000d81365592856454839f36fcdf62c0fa24c
Alex Burke <alex.burke@intercom.io>
2019-08-14 18:13:47 -0700

    Re-add seed methods and add members

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ To access the console in development:
 
 To run the server locally:
 `shotgun`
+
+rake db:create # Creates the database from DATABASE_URL or config/database.yml for...
+rake db:create_migration # Create a migration (parameters: NAME, VERSION)
+rake db:drop # Drops the database from DATABASE_URL or config/database.yml for t...
+rake db:fixtures:load # Load fixtures into the current environment's database
+rake db:migrate # Migrate the database (options: VERSION=x, VERBOSE=false, SCOPE=blog)
+rake db:migrate:status # Display status of migrations
+rake db:rollback # Rolls the schema back to the previous version (specify steps w/ S...
+rake db:schema:cache:clear # Clear a db/schema_cache.dump file
+rake db:schema:cache:dump # Create a db/schema_cache.dump file
+rake db:schema:dump # Create a db/schema.rb file that is portable against any DB suppor...
+rake db:schema:load # Load a schema.rb file into the database
+rake db:seed # Load the seed data from db/seeds.rb
+rake db:setup # Create the database, load the schema, and initialize with the see...
+rake db:structure:dump # Dump the database structure to db/structure.sql
+rake db:structure:load # Recreate the databases from the structure.sql file
+rake db:version # Retrieves the current schema version number
--- app/lib/admins_and_books.rb
+++ app/lib/admins_and_books.rb
@@ -32,6 +32,47 @@ class AdminsAndBooks
     Alex
   ]
 
+  BOOKS = {
+    season_1: [
+      "Cat's Cradle",
+      "I Invented The Modern Age",
+      "Under The Skin",
+      "How Music Works",
+      "Dream Team",
+      "Unbroken",
+      "Do Androids Dream of Electric Sheep",
+      "Women",
+      "Inherent Vice",
+      "Good Omens",
+      "Waking Up",
+      "The art of racing in the rain",
+    ],
+    season_2: [
+      "Watchmen",
+      "Predictably Irrational",
+      "The Wright Brothers",
+      "Ancillary Justice",
+      "All The Pretty Horses",
+      "Lafayette in the Somewhat United States",
+      "The Boys in the Boat",
+      "One Flew Over the Cuckoo's Nest",
+      "Redeployment",
+    ],
+    season_3: [
+      "A Constellation of Vital Phenomena",
+      "Dune",
+      "The River Why",
+      "The Glass Cage",
+      "Between the World and Me",
+      "In Patagonia",
+      "Hillbilly Elegy",
+      "The Control Of Nature",
+      "Killers of the Flower Moon",
+      "Fantasy Land",
+      "A Knight of the Seven Kingdoms",
+    ]
+  }
+
   def self.current_picker
     SEASON_FOUR_ORDER[CURRENT_PICKER_INDEX]
   end
@@ -58,6 +99,34 @@ class AdminsAndBooks
     end.compact
 
     order
+  end
+
+  def self.seed(persist: false)
+    all_books = Book.all
+    seasons = BOOKS.keys
+
+    seasons.each do |season|
+      BOOKS[season].each do |title|
+        season_number = season.to_s.last.to_i
+        existing_book = all_books.find_by(title: title)
+
+        if persist
+          if existing_book.present?
+            existing_book.update!(season: season_number)
+          else
+            Book.create(title: title, season: season_number)
+          end
+        else
+          print [season_number, title]
+        end
+      end
+    end
+  end
+
+  def self.seed_members
+    ADMINS.each do |name|
+      Member.create(name: name)
+    end
   end
 
 end
